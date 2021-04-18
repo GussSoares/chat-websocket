52aff27dcfd3a6e7e4b767ffc981fe7432c5be9b
GussSoares <gustavo.soares.cdc@gmail.com>
2021-04-18 13:35:13 -0300

    [14/04/2021][ :tada: ] refactore

--- main.py
+++ main.py
@@ -1,86 +1,97 @@
-from typing import List
+import json
+from typing import List, Optional
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi import (Cookie, Depends, FastAPI, Query, WebSocket,
+                     WebSocketDisconnect, status)
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+# Dar um jeito de projetar chat privado.
+# Talvez salvar no manager um objeto com um identificador do usuario
+# e o websocket associado. Assim, quando solicitar o acesso a conversa,
+# será usado o id do usuario apra saber qual socket deve ser usado.
 
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, client_id: str):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections.append({"websocket": websocket, "client": client_id})
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
+
+    def get_websocket_by_client_id(self, client_id: str):
+        for elem in self.active_connections:
+            if elem.get("client") == client_id:
+                return elem.get("websocket")
+
+    def get_online_users(self):
+        return [x.get("client") for x in self.active_connections]
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection.get("websocket").send_json({"message": message})
+
+    async def broadcast_json(self, data: dict):
+        for connection in self.active_connections:
+            await connection.get("websocket").send_json(data)
 
 
 manager = ConnectionManager()
+template = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def get():
-    return HTMLResponse(html)
+async def get(request: Request):
+    return template.TemplateResponse(name="chat.html", context={"request": request})
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
+# async def get_cookie_or_token(
+#     websocket: WebSocket,
+#     session: Optional[str] = Cookie(None),
+#     token: Optional[str] = Query(None),
+# ):
+#     if session is None and token is None:
+#         await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+#     return session or token
+
+
+@app.websocket("/ws/client/{client_id}")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    client_id: str,
+):
+    await manager.connect(websocket, client_id)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast_json({"users": manager.get_online_users()})
+            # await manager.broadcast(f"{client_id}: {data}")
 
     except WebSocketDisconnect:
-
         manager.disconnect(websocket)
+        await manager.broadcast(f"{client_id} saiu")
 
-        await manager.broadcast(f"Client #{client_id} left the chat")
+
+# import json
+# @app.websocket('/ws/get-users-online')
+# async def websocket_get_online(websocket: WebSocket):
+#     await manager.connect(websocket)
+#     try:
+#         while True:
+#             data = await websocket.receive()
+#             await manager.broadcast_json({'users': manager.get_online_users()})
+#             # await manager.broadcast('fulano entrou')
+#     except WebSocketDisconnect:
+#         manager.disconnect(websocket)
+#     except Exception as exc:
+#         print(exc)
